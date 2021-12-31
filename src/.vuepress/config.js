5092ac61733f0159b4179bce31dda4c44017f0ea
wsWangjinxu <1576520768@qq.com>
2021-12-31 17:20:47 +0800

    2022

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,6 @@
-const { description } = require('../../package')
+const {
+  description
+} = require('../../package')
 
 module.exports = {
   /**
@@ -16,9 +18,18 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', {
+      name: 'theme-color',
+      content: '#3eaf7c'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'black'
+    }]
   ],
 
   /**
@@ -32,37 +43,44 @@ module.exports = {
     docsDir: '',
     editLinkText: '',
     lastUpdated: false,
-    nav: [
-      {
-        text: 'JavaScript',
-        link: '/javascript/',
-      },
-      {
-        text: 'Rust',
-        link: '/rust/',
+    nav: [{
+        text: 'Blog',
+        link: '/blog/javascript/',
       },
       // {
       //   text: 'Config',
       //   link: '/config/'
       // },
       {
+        text: 'LeetCode',
+        link: 'https://leetcode-cn.com/u/wswangjinxu/'
+      },
+      {
         text: 'Github',
         link: 'https://github.com/wsWangjinxu'
       }
     ],
     sidebar: {
-      '/javascript/': [
-        {
-          title: 'JavaScript',
-          collapsable: false,
+      '/blog/': [{
+          title: 'JavaScript', // 必要的 // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
           children: [
-            '',
-            'this',
-            '闭包必知必会',
-            '内存管理与垃圾回收'
+            'javascript/',
+            'javascript/this',
+            'javascript/闭包必知必会',
+            'javascript/内存管理与垃圾回收'
+          ]
+        },
+        {
+          title: 'Rust', // 必要的 // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          children: [
+            'rust/'
           ]
         }
-      ],
+      ]
       // '/guide/': [
       //   {
       //     title: 'Guide',
--- src/index.md
+++ src/index.md
@@ -1,15 +1,16 @@
 ---
 home: true
-heroImage: /shanyi.jpeg
-tagline: 
-actionText: 壹之型 →
-actionLink: /guide/
-features:
-- title: 六连
-  details: Feature 1 Description
-- title: 八连
-  details: Feature 2 Description
-- title: 神速
-  details: Feature 3 Description
+heroImage: /2022.jpeg
+tagline: 喝不上一碗胡辣汤
+# actionText: 壹之型 →
+actionText: Enter →
+actionLink: /blog/javascript/
+# features:
+# - title: 六连
+#   details: 算法与数据结构
+# - title: 八连
+#   details: 前端
+# - title: 神速
+#   details: rust
 footer: Made by  with thrownewerror
 ---
